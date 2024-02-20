e01a94e015847074c8982b3604caf45481651474
9alone <160559077+9alone@users.noreply.github.com>
2024-02-19 18:55:05 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    CyBit.TimePAUSE(5000)
+    start()
+})
+function doSomething () {
+    while (true) {
+        if (CyBit.analogRead(CyBit.analogPort.A0) < 0 && (CyBit.analogRead(CyBit.analogPort.A1) < 0 && (CyBit.analogRead(CyBit.analogPort.A2) < 0 && CyBit.analogRead(CyBit.analogPort.A3) < 0))) {
+            CyBit.Motor_Stop(motorCH.M1234)
+            break;
+        }
+        CyBit.PID(
+        Transmissions.Haft,
+        50,
+        0,
+        0,
+        [0, 1]
+        )
+    }
+}
+function turn () {
+    CyBit.Motor_spin(motorTurn.Left, 40)
+    CyBit.TimePAUSE(1200)
+    CyBit.Motor_Stop(motorCH.M1234)
+}
 function ค่าแสง () {
     OLED12864_I2C.init(60)
     OLED12864_I2C.showNumber(
